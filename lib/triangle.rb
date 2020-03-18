6dd4b9995b5887562dcfd2ae55eac905fbb0396b
Brandon Dusch <brandondusch@gmail.com>
2020-03-18 19:09:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,84 +1,9 @@
-require 'pry'
-
-class Triangle
-  # write code here
-  attr_accessor :length1, :length2, :length3, :type
+class Triangle 
+  attr_accessor :A, :B, :C 
   
-  def initialize(length1, length2, length3)
-    @length1 = length1
-    @length2 = length2
-    @length3 = length3
+  def initialize(A, B, C)
+    @A = A 
+    @B = B 
+    @C = C 
   end
-  
-  
-  def kind 
-    if(self.triangle_inequality?)
-      if(self.equilateral?)
-        @type = :equilateral
-        @type
-      end
-      
-      if(self.isosceles?)
-        @type = :isosceles
-        @type
-      end
-      
-      if(self.scalene?)
-        @type = :scalene
-        @type
-      end
-    else 
-      if(length1 <= 0 || length2 <= 0 || length3 <= 0)
-        raise TriangleError
-      else
-      end
-    end
-  end
-  
-  def triangle_inequality?
-    if((self.length1 + self.length2 >= self.length3) ||
-       (self.length2 + self.length3 >= self.length1) ||
-       (self.length1 + self.length3 >= self.length2))
-      true
-    else
-      raise TriangleError
-    end
-  end
-  
-  def equilateral?
-    if(self.length1 == self.length2 && self.length1 == self.length3)
-      true 
-    else 
-      false
-    end
-  end
-  
-  def isosceles?
-    if(self.length1 == self.length2 || self.length1 == self.length3 || self.length2 == self.length3)
-      true
-    else 
-      false
-    end
-  end
-    
-  def scalene?
-    if(self.length1 != self.length2 && self.length1 != self.length3 && self.length2 != self.length3)
-      true
-    else 
-      false
-    end
-  end
-  
-  
-  class TriangleError < StandardError
-    def message 
-      "Invalid Triangle"
-    end
-  end
-  
 end
-
-
-#<Triangle:0x00000000014382e8 @length1=4, @length2=3, @length3=4>
-triangle = Triangle.new(4, 3, 4)
-binding.pry
