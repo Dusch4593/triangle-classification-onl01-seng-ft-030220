2d2301e3e592d4a352d9089289b5e8b9d54638ff
Brandon Dusch <brandondusch@gmail.com>
2020-03-18 18:59:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,74 @@
+require 'pry'
+
 class Triangle
   # write code here
+  attr_accessor :length1, :length2, :length3
+  
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+  
+  
+
+
+  def kind 
+    if(self.triangle_inequality?)
+      if(self.equilateral?)
+        :equilateral
+      end
+      
+      if(self.isosceles?)
+        :isosceles 
+      end
+      
+      if(self.scalene?)
+        :scalene 
+      end
+    else 
+      if(length1 <= 0 || length2 <= 0 || length3 <= 0)
+        raise TriangleError
+      else
+      end
+    end
+  end
+  
+  def triangle_inequality?
+    if((self.length1 + self.length2 >= self.length3) ||
+       (self.length2 + self.length3 >= self.length1) ||
+       (self.length1 + self.length3 >= self.length2))
+      true
+    else
+      raise TriangleError
+    end
+  end
+  
+  def equilateral?
+    if(self.length1 == self.length2 && self.length1 == self.length3)
+      true 
+    end
+  end
+  
+  def isosceles?
+    if(self.length1 == self.length2 || self.length1 == self.length3 || self.length2 == self.length3)
+      true
+    end
+  end
+    
+  def scalene?
+    if(self.length1 != self.length2 && self.length1 != self.length3 && self.length2 != self.length3)
+      true 
+    end
+  end
+  
+  
+  class TriangleError < StandardError
+    def message 
+      "Invalid Triangle"
+    end
+  end
+  
 end
+
+# binding.pry
