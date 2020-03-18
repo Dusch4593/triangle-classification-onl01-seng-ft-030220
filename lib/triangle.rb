aba4d417ee32e9d66e03bff1f6e599cb808f2261
Brandon Dusch <brandondusch@gmail.com>
2020-03-18 19:28:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -29,7 +29,7 @@ class Triangle
       raise TriangleError 
     else 
       if(self.triangle_inequality?)
-        binding.pry
+        
       else 
         raise TriangleError
       end
@@ -48,3 +48,5 @@ class Triangle
     end
   end
 end
+
+binding.pry
